ae5e2516387dcc9e1d55603a25fdf23ec09b5695
Josue Alexander Cayetano <josue_cayetano@claro.com.do>
2019-04-30 09:07:26 -0400

    Tipos de datos

---
+++ Angujar7/Fundamentos/app1.js
@@ -0,0 +1,12 @@
+"use strict";
+// Tipos de datos en Typescript
+//string 
+var texto = "soy una cadena";
+var numero = 1234;
+var truefalse = false;
+var cualquiera = "Cualquier cosa";
+//Arrays
+var Arraycadenas = ["cadena1", "cadena2"]; //Arrego de strings
+var Arraynumber = [1, 3, 5, 8];
+var Arrayboolean = [true, false, false, true, true];
+var Arrayany = ["texto", 123, true, false];
---
+++ Angujar7/Fundamentos/app1.ts
@@ -0,0 +1,14 @@
+// Tipos de datos en Typescript
+
+//string 
+ let texto: string = "soy una cadena";
+ let numero: number = 1234;
+ let truefalse : boolean = false;
+ let cualquiera : any = "Cualquier cosa";
+
+ //Arrays
+
+ let Arraycadenas : string[] = ["cadena1", "cadena2"]; //Arrego de strings
+ let Arraynumber: number []= [1,3,5,8];
+ let Arrayboolean : boolean[]= [true, false, false, true, true];
+ let Arrayany: any[]= ["texto", 123, true, false];
